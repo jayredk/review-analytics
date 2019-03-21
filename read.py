249380d4f8ef4cfa0c8f336515961b0da24e2d37
Jay <kred110396@gmail.com>
2019-03-21 22:43:16 +0800

    add avg len

--- read.py
+++ read.py
@@ -7,3 +7,8 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0:
 			print(len(data))
 print('讀取完畢，總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+print('留言平均長度為：', sum_len / len(data))
